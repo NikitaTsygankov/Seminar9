e0f93e63807c75ced5fdf1ca2f6771221b8d8ca1
Nikita Tsygankov <like_fender@mail.ru>
2023-04-20 22:44:07 +0300

    task123 added

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+
+int NumSum(int arg)
+{
+   int result = arg % 10;
+   if (arg/10 > 0)
+    {
+        return result + NumSum(arg/10);
+    }
+    return result;
+}
+Console.WriteLine(NumSum(256));
